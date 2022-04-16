76678498627dec0f8069d36d5cdb6ca28a93e670
[ismaylibra] <[ismayiliib@code.edu.az]>
2022-04-16 16:49:50 +0400

    added task 1 adding a number at digit's begining

---
+++ Homework 1 18.04.2022/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Homework_1_18._04._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Eded daxil edin: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            Add(a);
+        }  public static void Add(int num )
+        {
+            int a = num;
+            int count = 0;
+            Console.Write("Elave olunacaq eded: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            while (num != 0)
+            {
+                num /= 10;
+                count++;
+            }
+            
+            Console.Write("Yeni eded: ");
+            Console.Write(num2 * (Math.Pow(10, count++)) + a);
+        }
+    } 
+        
+
+    }
+
